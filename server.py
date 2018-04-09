3b6a790013c23c37beb6bd8f8e496b424b976c2a
justpd <31624803+justpd@users.noreply.github.com>
2018-04-10 00:44:27 +0700

    Multithread + OOP

--- server.py
+++ server.py
@@ -1,46 +1,44 @@
 import socket
 import json
+import threading
 from Handler import HandleData
 
-# Инициализируем сокет
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.bind(('192.168.1.8', 8867 ))
-print('Server > 192.168.1.8:8867')
+# Создаём класс, контролирующий закачку больших данных
+class Storage():
+    
+    # Инициализируем хранилище для подкачки данных клиента
+    def __init__(self):
+        self.data = bytes()
+        self.size = 0
+    
+    # Проверям завершилась ли подкачка
+    def is_done(self):
+        return len(self.data) == self.size
+    
 
-# Создаём пустой словарь для хранения списка клиентов key = (ip,port), value = socket
-clients = {}
-
-# Создаём переменные, контролирующие закачку больших данных
-storage = bytes()
-loadingSize = 0
-loading = False
-
-# Начинаем прослушивать входящие соединения
-server_socket.listen(10)
-
-# Основной цикл обработки соединений
-while True:
-    try:
-        # Получаем сокет и адресс (ip, port) клиента, который подключился
-        client_socket, addr = server_socket.accept()
-
-        # Если клиента еще нет в списке онлайн клиентов - добавляем его
-        if addr not in clients:
-            clients[addr] = client_socket
-            print(clients)
-            print('{}:{} connected.'.format(addr[0], addr[1]))
-        
+class Server():
+    
+    # Инизиализируем сервер
+    def __init__(self, ip, port):
+        # Инициализируем сокет
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.bind((ip, port))
+        print('Server > running on {}:{}'.format(ip, port))
+    
+    # Отдельный поток обработки данных для клиента
+    def data_thread(self, client_socket, client_addres):
+        storage = Storage()
         # Цикл считывания данных от клиента через его client_socket
         while True:
             # Получаем данные
-            data = client_socket.recv(4096)
-            
+            data = client_socket.recv(2048)
+
             # Проверка на целостность данных
             if not data:
                 break
 
             # Если сейчас не идет подгрузка файла от этого клиента, то обрабатываем новые запросы
-            if (not loading):
+            if (storage.size == 0):
                 # Данные, которые мы получаем состоят из трёх частей:
                 # > Ключ пакета (4 байта)
                 # > Размер чистых данных, не учитывая ключ и саму информацию о размере (4 байта)
@@ -59,40 +57,68 @@ while True:
                     HandleData(client_socket, index, size, data)
                 # Иначе - начинаем подкачку данных
                 else:
-                    loading = True
-                    loadingSize = size
-                    storage += bytes(data)
-
+                    storage.size = size
+                    storage.data += bytes(data)
             # Иначе - получаем данные текущей подгрузки
             else:
-                storage += bytes(data)
+                storage.data += bytes(data)
                 # Если длинна полученных данных равна размеру подкачиваемого файла > файл успешно загружен
-                if (len(storage) == loadingSize):
+                if (storage.is_done()):
                     print('Load finished..')
 
-                    index = int.from_bytes(bytes(storage[0:4]), byteorder='little')
-                    data = bytes(storage[8:]).decode("utf-8")
-                    HandleData(client_socket, index, loadingSize, data)
+                    index = int.from_bytes(bytes(storage.data[0:4]), byteorder='little')
+                    data = bytes(storage.data[8:]).decode("utf-8")
+                    HandleData(client_socket, index, storage.size, data)
 
-                    storage = bytes()
-                    loading = False
-                    loadingSize = 0
-                
+                    storage = Storage()
                 # Иначе - выводим состояние подкачки
                 else:
-                    percent = len(storage) / loadingSize
+                    percent = len(storage.data) / storage.size
                     print('Loading ..... ({:.2f}%)'.format(percent * 100))
     
-    # При разрыве соединения с сокетом клиента
-    except Exception as e:
-        client_socket.close()
-        try:
-            del clients[addr]
-        except KeyError:
-            pass
-        print('{}:{} disconnected.'.format(addr[0], addr[1]))
+    # Запускаем шарманку
+    def start(self):
+        # Создаём пустой словарь для хранения списка клиентов key = (ip,port), value = socket
+        self.clients = {}
+        # Начинаем прослушивать входящие соединения
+        self.server_socket.listen(10)
 
-        print(e)
+        # Основной цикл обработки соединений
+        while True:
+            try:
+                # Получаем сокет и адресс (ip, port) клиента, который подключился
+                client_socket, client_addres = self.server_socket.accept()
 
-# При сбое на сервере закрываем свой сокет
-server_socket.close()
+                # Если клиента еще нет в списке онлайн клиентов - добавляем его
+                if client_addres not in self.clients:
+                    self.clients[client_addres] = client_socket
+                    print(self.clients)
+                    print('{}:{} connected.'.format(client_addres[0], client_addres[1]))
+
+                # Запускаем новый поток для обработки данных клиента
+                threading.Thread(target=self.data_thread, args=(client_socket, client_addres)).start()
+
+            # При разрыве соединения с сокетом клиента
+            except Exception as e:
+                client_socket.close()
+                try:
+                    del self.clients[client_addres]
+                except KeyError:
+                    pass
+                print('{}:{} disconnected.'.format(client_addres[0], client_addres[1]))
+
+                print(e)
+    
+    # Сворачиваемся
+    def close(self):
+        self.server_socket.close()
+
+def Main():
+    print('Starting server on:')
+    ip = input('ip: ')
+    port = int(input('port: '))
+    server = Server(ip, port)
+    server.start()
+
+if __name__ == '__main__':
+    Main()
