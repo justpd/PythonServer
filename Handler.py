850160211e6a2ed98eab2e7545bdce109cf440bd
justpd <31624803+justpd@users.noreply.github.com>
2018-04-09 15:21:18 +0700

    Message fix

--- Handler.py
+++ Handler.py
@@ -1,9 +1,17 @@
 from Sender import *
+import base64
+import json
 
 def HandleData(socket, key, size, data):
     addr = socket.getpeername()
     print('({}) {}:{} > {}'.format(size, addr[0], addr[1], key))
 
-    # if (key == 505001):
-    #     SendData(socket, 606001, "Welcome to our server!")
+    jsonData = json.loads(data)
+
+    if (jsonData['b64str']):
+        with open(jsonData['login'] + '.png', "wb") as fh:
+            fh.write(base64.decodebytes(jsonData['b64str'].encode('utf-8')))
+
+    if (key == 505001):
+        SendData(socket, 606001, "Welcome to our server!")
 
--- Sender.py
+++ Sender.py
@@ -1,8 +1,5 @@
 def SendData(socket, key, data):
     msg = data.encode('utf-8')
-    print(msg)
-    print(len(msg))
     data = key.to_bytes(4, 'little') + (len(msg)).to_bytes(4, 'little') + msg
-    print(data)
-    socket.send(len(msg).to_bytes(4, 'little'))
+    socket.send((len(msg) + 8).to_bytes(4, 'little'))
     socket.send(data)
