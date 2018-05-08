0d6051d7c20998af18e549bf8d67457b1d0bb055
justpd <31624803+justpd@users.noreply.github.com>
2018-05-09 00:35:40 +0700

    PTM

--- Handler.py
+++ Handler.py
@@ -8,9 +8,9 @@ def HandleData(socket, key, size, data):
 
     jsonData = json.loads(data)
 
-    if (jsonData['b64str']):
-        with open(jsonData['login'] + '.png', "wb") as fh:
-            fh.write(base64.decodebytes(jsonData['b64str'].encode('utf-8')))
+    # if (jsonData['b64str']):
+    #     with open(jsonData['login'] + '.png', "wb") as fh:
+    #         fh.write(base64.decodebytes(jsonData['b64str'].encode('utf-8')))
 
     if (key == 505001):
         SendData(socket, 606001, "Welcome to our server!")
